d749d96200099f1e0723071874a888f879a5de27
Jean Felipe <jeangg9393@gmail.com>
2024-05-01 15:02:37 -0300

    Loop principal do jogo criado e primeiro print de elemento na tela

--- TurnBasedRPG/src/game/GamePanel.java
+++ TurnBasedRPG/src/game/GamePanel.java
@@ -3,8 +3,10 @@ package game;
 import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
-public class GamePanel extends JPanel{
+public class GamePanel extends JPanel implements Runnable{
 	
 	final int width = 320;
 	final int height = 180;
@@ -13,11 +15,51 @@ public class GamePanel extends JPanel{
 	final int screenWidth = width * scale;
 	final int screenHeight = height * scale;
 	
+	Thread gameThread;
+	
 	public GamePanel() {
 		
 		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
 		this.setBackground(Color.black);
 		this.setDoubleBuffered(true);
 	}
+	
+	public void startGameThread() {
+		
+		gameThread = new Thread(this);
+		gameThread.start();
+		
+	}
+
+	public void run() {
+		
+		while (gameThread != null) {
+			
+			System.out.println("Jogo rodando!");
+			updateInfoOnScreen(); // recebe o comando do player e passa para a screen
+			repaint(); // chamando o método 'paintComponent'
+			
+		}
+		
+	}
+	
+	public void updateInfoOnScreen() {
+		
+	}
+	
+	public void paintComponent(Graphics g) {
+		
+		super.paintComponent(g);
+		
+		Graphics2D g2D = (Graphics2D)g; // convertendo os gráficos p/ 2D propriamente
+		
+		g2D.setColor(Color.green);
+		
+		g2D.fillRect(100, 100, 48, 48);
+		
+		g2D.dispose();
+	}
+	
+	
 	
 }
