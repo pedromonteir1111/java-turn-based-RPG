1062b53809d0abea49fec6379bde1961f77ddbf7
pedromonteir1111 <pmfm2004@gmail.com>
2024-06-16 13:19:43 -0300

    entrar em combate, selecionar quadrados e mover player com mouse

--- TurnBasedRPG/src/combat/Square.java
+++ TurnBasedRPG/src/combat/Square.java
@@ -5,17 +5,28 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
+import enemies.Enemy;
+import playerClasses.Player;
+
 public class Square {
 	
 	private BufferedImage image;
 	private int x;
 	private int y;
-	private int squareSize;
+	private int relativeX;
+	private int relativeY;
+
+	private String currentState;
 	
-	public Square(int x, int y, int squareSize) {
+	private Player currentPlayer;
+	private Enemy currentEnemy;
+	
+	public Square(int x, int y, int relativeX, int relativeY) {
 		this.x = x;
 		this.y = y;
-		this.squareSize = squareSize;
+		this.relativeX = relativeX;
+		this.relativeY = relativeY;
+		this.currentState = "";
 		
 		try {
 			this.image = ImageIO.read(getClass().getResourceAsStream("/tiles/square.png"));
@@ -23,8 +34,65 @@ public class Square {
 			e.printStackTrace();
 		}
 	}
+	
+	public void setImage(String state) {
+		
+		switch (state) {
+			case "Hover":
+				if(!currentState.equals("Selected")) {
+					currentState = state;
+				}
+				break;
+				
+			case "Selected":
+				currentState = state;
+				break;
+				
+			case "":
+				currentState = state;
+				break;
+		}
+		
+		try {
+			this.image = ImageIO.read(getClass().getResourceAsStream("/tiles/square" + currentState + ".png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 	public BufferedImage getImage() {
 		return image;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public Player getCurrentPlayer() {
+		return currentPlayer;
+	}
+
+	public void setCurrentPlayer(Player currentPlayer) {
+		this.currentPlayer = currentPlayer;
+	}
+
+	public Enemy getCurrentEnemy() {
+		return currentEnemy;
+	}
+
+	public void setCurrentEnemy(Enemy currentEnemy) {
+		this.currentEnemy = currentEnemy;
+	}
+
+	public int getRelativeX() {
+		return relativeX;
+	}
+
+	public int getRelativeY() {
+		return relativeY;
 	}
 }
